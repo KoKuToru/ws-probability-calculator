35504b838e8d33553c48d3fc4b46b830e6b59868
Luca Béla Palkovics <luca.bela.palkovics@gmail.com>
2023-08-27 10:56:38 +0200

    working code-validation

---
+++ src/app/components/code-validation.js
@@ -0,0 +1,19 @@
+import Component from '@glimmer/component';
+
+const syntax = [
+  /^attack\s+([0-9]+)\s*$/g,
+  /^burn\s+([0-9]+)\s*$/g
+];
+
+export default class CodeValidation extends Component {
+  get class() {
+    const value = this.args.value;
+    for (const s of syntax) {
+      s.lastIndex = 0; //< reset the regex
+      if (s.exec(value)) {
+        return 'ok'
+      }
+    }
+    return 'bad';
+  }
+}

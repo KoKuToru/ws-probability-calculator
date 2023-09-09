c81a5170740522b3b32f754fb4a3f7342f0d07ed
Luca Béla Palkovics <luca.bela.palkovics@gmail.com>
2023-09-09 09:31:02 +0200

    fix mill step generation

--- src/public/worker/actions/mill.js
+++ src/public/worker/actions/mill.js
@@ -9,8 +9,8 @@ export default class Mill extends Action {
 
 function *mill(count) {
   // all permutations
-  for (let n = 0; n < count; ++n) {
-    const cards = NOT_CX.repeat(n) + CX.repeat(count - n - 1);
+  for (let n = 0; n <= count; ++n) {
+    const cards = NOT_CX.repeat(n) + CX.repeat(count - n);
     for (let p of permute(cards)) {
       yield Step.create({
         op: p,

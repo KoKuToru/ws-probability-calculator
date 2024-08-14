065d03a5cf13a58e3f5f2bddf08e3e0887bf5466
Luca Béla Palkovics <luca.bela.palkovics@gmail.com>
2024-08-14 20:36:29 +0200

    each_reshuffle: reuse each_permutation

--- cpp/utils/each_reshuffle.hpp
+++ cpp/utils/each_reshuffle.hpp
@@ -6,36 +6,9 @@
 
 #include <iostream>
 
-struct ReshuffleState {
-    void reset() {
-        idx = 1;
-    }
-    void next() {
-        idx = 0;
-        state += 1;
-    }
-    bool check() {
-        assert(idx != 0); //<- check if reset got called
-        const auto v = state & idx;
-        mask |= idx;
-        idx <<= 1;
-        return v;
-    }
-    bool done() {
-        return (state & mask) == 0;
-    }
+#include "each_permutation.hpp"
 
-    ReshuffleState() {}
-    ReshuffleState(ReshuffleState&&) = delete;
-    ReshuffleState(const ReshuffleState&) = delete;
-    ReshuffleState& operator=(ReshuffleState&&) = delete;
-    ReshuffleState& operator=(const ReshuffleState&) = delete;
-
-    private:
-    uint64_t state = 0;
-    uint64_t idx   = 0;
-    uint64_t mask  = 0;
-};
+struct ReshuffleState: PermutationState {};
 
 template<typename F>
 concept reshuffle_callback = requires(F f, ReshuffleState& r) {
